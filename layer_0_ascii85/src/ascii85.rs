facff784face0f9146475e84b0b049c680ccaf87
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-07-16 00:11:15 +0100

    Layer 0: move methods around so in logical order

--- layer_0_ascii85/src/ascii85.rs
+++ layer_0_ascii85/src/ascii85.rs
@@ -4,9 +4,41 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn parse_file(path_to_input: PathBuf) -> Vec<u8> {
-    let input_str = extract_encoded_text(get_file_string(path_to_input));
+    let input_str = extract_encoded_text(read_file(path_to_input).unwrap());
 
     decode_ascii85(input_str)
+}
+
+fn read_file(filename: PathBuf) -> Result<String, Box<dyn Error>> {
+    let input = fs::read_to_string(filename)?;
+    Ok(input)
+}
+
+fn extract_encoded_text(input_str: String) -> String {
+    let mut string_start = false;
+    let mut string_end = false;
+    let mut start_point: usize = 0;
+    let mut end_point: usize = input_str.len();
+
+    for (i, c) in input_str.chars().enumerate() {
+        if c == '<' {
+            string_start = true;
+        } else if c == '~' && string_start {
+            start_point = i + 1;
+        } else if c == '~' {
+            string_end = true;
+        } else if c == '>' && string_end {
+            end_point = i - 1;
+        } else {
+            string_start = false;
+            string_end = false;
+        }
+    }
+
+    // Convert to single line
+    input_str[start_point..end_point]
+        .lines()
+        .collect::<String>()
 }
 
 pub fn decode_ascii85(input_str: String) -> Vec<u8> {
@@ -45,42 +77,6 @@ fn bit_32_to_bytes(value: u32) -> Vec<u8> {
         bytes.push(byte_value);
     }
     bytes
-}
-
-fn read_file(filename: PathBuf) -> Result<String, Box<dyn Error>> {
-    let input = fs::read_to_string(filename)?;
-    Ok(input)
-}
-
-fn get_file_string(path_to_input: PathBuf) -> String {
-    read_file(path_to_input).unwrap()
-}
-
-fn extract_encoded_text(input_str: String) -> String {
-    let mut string_start = false;
-    let mut string_end = false;
-    let mut start_point: usize = 0;
-    let mut end_point: usize = input_str.len();
-
-    for (i, c) in input_str.chars().enumerate() {
-        if c == '<' {
-            string_start = true;
-        } else if c == '~' && string_start {
-            start_point = i + 1;
-        } else if c == '~' {
-            string_end = true;
-        } else if c == '>' && string_end {
-            end_point = i - 1;
-        } else {
-            string_start = false;
-            string_end = false;
-        }
-    }
-
-    // Convert to single line
-    input_str[start_point..end_point]
-        .lines()
-        .collect::<String>()
 }
 
 pub fn bytes_to_ascii(bytes: Vec<u8>) -> String {

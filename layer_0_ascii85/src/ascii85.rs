ec33e7c84a4f0d741d990f7e761ee72e6a5f38b7
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-07-16 00:05:28 +0100

    Layer 0: tidy up so output bytes instead of 32-bit chunks

--- layer_0_ascii85/src/ascii85.rs
+++ layer_0_ascii85/src/ascii85.rs
@@ -3,47 +3,48 @@ use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn parse_file(path_to_input: PathBuf) -> Vec<u32> {
-    let input_str = get_encoded_text(get_file_string(path_to_input));
+pub fn parse_file(path_to_input: PathBuf) -> Vec<u8> {
+    let input_str = extract_encoded_text(get_file_string(path_to_input));
 
     decode_ascii85(input_str)
 }
 
-pub fn decode_ascii85(input_str: String) -> Vec<u32> {
-    let mut output_32_bits = Vec::new();
+pub fn decode_ascii85(input_str: String) -> Vec<u8> {
+    let mut bytes = Vec::new();
 
     let mut padded_str = input_str;
     while padded_str.len() % 5 != 0 {
         padded_str.push('u')
     }
 
-    let mut value: u32 = 0;
+    let mut bit_32_value: u32 = 0;
     for (i, c) in padded_str.chars().enumerate() {
         let power = (4 - (i % 5)) as u32;
         let character_value = c as u32 - 33;
-        value = value + (character_value * (85u32).pow(power));
+        bit_32_value = bit_32_value + (character_value * (85u32).pow(power));
 
         if i % 5 == 4 {
-            output_32_bits.push(value);
-            value = 0;
+            bytes.extend(bit_32_to_bytes(bit_32_value));
+            bit_32_value = 0;
         }
     }
 
-    output_32_bits
+    bytes
 }
 
-fn value_to_ascii(value: u32) -> String {
-    let mut output_str = String::new();
+fn bit_32_to_bytes(value: u32) -> Vec<u8> {
+    let mut bytes = Vec::new();
     for i in (0..4).rev() {
-        let ascii_number: u32 = (value / (256u32).pow(i)) % 256;
-        if ascii_number == 3 {
+        let byte_value: u8 = ((value / (256u32).pow(i)) % 256) as u8;
+
+        // byte 3 is end of text character
+        if byte_value == 3 {
             break;
         }
 
-        let ascii_char = char::from_u32(ascii_number).unwrap();
-        output_str.push(ascii_char);
+        bytes.push(byte_value);
     }
-    output_str
+    bytes
 }
 
 fn read_file(filename: PathBuf) -> Result<String, Box<dyn Error>> {
@@ -55,7 +56,7 @@ fn get_file_string(path_to_input: PathBuf) -> String {
     read_file(path_to_input).unwrap()
 }
 
-fn get_encoded_text(input_str: String) -> String {
+fn extract_encoded_text(input_str: String) -> String {
     let mut string_start = false;
     let mut string_end = false;
     let mut start_point: usize = 0;
@@ -82,10 +83,11 @@ fn get_encoded_text(input_str: String) -> String {
         .collect::<String>()
 }
 
-pub fn bits_32_to_ascii(bits_32: Vec<u32>) -> String {
+pub fn bytes_to_ascii(bytes: Vec<u8>) -> String {
     let mut output_str = String::new();
-    for b in bits_32 {
-        output_str = format!("{}{}", output_str, value_to_ascii(b));
+    for b in bytes {
+        let ascii_char = char::from_u32(b as u32).unwrap();
+        output_str.push(ascii_char);
     }
     output_str
 }
--- layer_0_ascii85/src/main.rs
+++ layer_0_ascii85/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 const PATH_TO_INPUT: &str = "./layer_0.txt";
 
 fn main() {
-    let output_str = ascii85::parse_file(fs::canonicalize(PATH_TO_INPUT).unwrap());
+    let bytes_vec = ascii85::parse_file(fs::canonicalize(PATH_TO_INPUT).unwrap());
 
-    fs::write("output.txt", ascii85::bits_32_to_ascii(output_str)).expect("Unable to write file");
+    fs::write("output.txt", ascii85::bytes_to_ascii(bytes_vec)).expect("Unable to write file");
 }

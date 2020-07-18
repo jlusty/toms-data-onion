d928e8aeb61b44f0e366058de4477a23b209624c
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-07-18 19:37:58 +0100

    Layer 4: Add boilerplate

---
+++ layer_4_network/src/main.rs
@@ -0,0 +1,13 @@
+use layer_0_ascii85::ascii85::{bytes_to_ascii, parse_file};
+use std::fs;
+
+fn main() {
+    let bytes = parse_file(fs::canonicalize("./layer_4.txt").unwrap());
+
+    let mut new_bytes = Vec::new();
+    for b in bytes {
+        new_bytes.push(b);
+    }
+
+    fs::write("output.txt", bytes_to_ascii(new_bytes)).expect("Unable to write file");
+}

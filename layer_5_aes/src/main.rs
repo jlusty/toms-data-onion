fd67de0c7c119c43d0b2f313d5c58ea2b6e0fd3d
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-07-20 20:57:40 +0100

    Layer 5: Attempt to use OpenSSL to decrypt AES

--- layer_5_aes/src/main.rs
+++ layer_5_aes/src/main.rs
@@ -1,15 +1,47 @@
 use layer_0_ascii85::ascii85::{bytes_to_ascii, parse_file};
+use openssl::aes::{unwrap_key, AesKey};
+use openssl::symm::{decrypt, Cipher};
+use std::convert::TryInto;
 use std::fs;
 
 fn main() {
     let bytes = parse_file(fs::canonicalize("./layer_5.txt").unwrap());
 
-    let mut new_bytes = Vec::new();
     let mut i = 0;
-    while i < bytes.len() {
-        new_bytes.push(bytes[i]);
-        i += 1;
-    }
+    // First 32 bytes: The 256-bit key encrypting key (KEK).
+    let kek = &bytes[i..i + 32];
+    i += 32;
+    // Next 8 bytes: The 64-bit initialization vector (IV) for the wrapped key.
+    let key_iv: [u8; 8] = (&bytes[i..i + 8]).try_into().unwrap();
+    i += 8;
+    // Next 40 bytes: The wrapped (encrypted) key. When decrypted, this will become the 256-bit encryption key.
+    let key = &bytes[i..i + 40];
+    i += 40;
+    // Next 16 bytes: The 128-bit initialization vector (IV) for the encrypted payload.
+    let payload_iv = &bytes[i..i + 16];
+    i += 16;
+    // All remaining bytes: The encrypted payload.
+    let payload = &bytes[i..bytes.len()];
 
-    fs::write("output.txt", bytes_to_ascii(new_bytes)).expect("Unable to write file");
+    println!("KEK: {:?}", kek);
+    println!("Key IV: {:?}", key_iv);
+    println!("Key: {:?}", key);
+
+    let decrypted_key: &mut [u8] = &mut [];
+    let aes_kek = &AesKey::new_decrypt(kek).expect("Unable to create AesKey");
+    // Decrypt KEK
+    let bytes_in_key = unwrap_key(aes_kek, Some(key_iv), decrypted_key, key).unwrap();
+    println!("Bytes in key: {}", bytes_in_key);
+    println!("Decrypted key: {:?}", decrypted_key);
+
+    // Decrypt payload
+    let decrypted_payload = decrypt(
+        Cipher::aes_256_cbc(),
+        &decrypted_key,
+        Some(payload_iv),
+        payload,
+    )
+    .unwrap();
+
+    fs::write("output.txt", bytes_to_ascii(decrypted_payload)).expect("Unable to write file");
 }

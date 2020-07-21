9cee420e36c1e6f1f541a7071970699ba846f000
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-07-21 23:26:44 +0100

    Layer 6: Read "real" input

--- layer_6_vm/src/main.rs
+++ layer_6_vm/src/main.rs
@@ -17,57 +17,7 @@ struct Registers {
 }
 
 fn main() {
-    // let bytes = parse_file(fs::canonicalize("./layer_6.txt").unwrap());
-    #[rustfmt::skip]
-    let bytes = vec![
-        0x50, 0x48, 
-        0xC2, 
-        0x02, 
-        0xA8, 0x4D, 0x00, 0x00, 0x00, 
-        0x4F, 
-        0x02, 
-        0x50, 0x09, 
-        0xC4, 
-        0x02,
-        0x02, 
-        0xE1, 0x01, 
-        0x4F, 
-        0x02, 
-        0xC1, 
-        0x22, 0x1D, 0x00, 0x00, 0x00, 
-        0x48, 0x30, 
-        0x02, 
-        0x58, 0x03,
-        0x4F,
-        0x02, 
-        0xB0, 0x29, 0x00, 0x00, 0x00, 
-        0x48, 0x31, 
-        0x02, 
-        0x50, 0x0C, 
-        0xC3, 
-        0x02,
-        0xAA, 
-        0x57, 
-        0x48, 0x02, 
-        0xC1, 
-        0x21, 0x3A, 0x00, 0x00, 0x00, 
-        0x48, 0x32, 
-        0x02, 
-        0x48, 0x77,
-        0x02, 
-        0x48, 0x6F, 
-        0x02, 
-        0x48, 0x72, 
-        0x02, 
-        0x48, 0x6C, 
-        0x02, 
-        0x48, 0x64, 
-        0x02, 
-        0x48, 0x21,
-        0x02, 
-        0x01, 
-        0x65, 0x6F, 0x33, 0x34, 0x2C,
-    ];
+    let bytes = parse_file(fs::canonicalize("./layer_6.txt").unwrap());
 
     let mut r = Registers {
         a: 0,
@@ -98,11 +48,11 @@ fn main() {
                 let dest = (i & 0b00111000) >> 3;
                 let src = i & 0b00000111;
                 if src != 0 {
-                    println!("MV {{dest}} <- {{src}}")
+                    // println!("MV {{dest}} <- {{src}}")
                 }
                 let src_val = match src {
                     0 => {
-                        println!("MVI {{dest}} <- imm8");
+                        // println!("MVI {{dest}} <- imm8");
                         let src_val = memory[r.pc as usize];
                         r.pc += 1;
                         src_val
@@ -131,11 +81,11 @@ fn main() {
                 let dest = (i & 0b00111000) >> 3;
                 let src = i & 0b00000111;
                 if src != 0 {
-                    println!("MV32 {{dest}} <- {{src}}");
+                    // println!("MV32 {{dest}} <- {{src}}");
                 }
                 let src_val = match src {
                     0 => {
-                        println!("MVI32 {{dest}} <- imm32");
+                        // println!("MVI32 {{dest}} <- imm32");
                         let src_val = get_imm32(&memory, r.pc);
                         r.pc += 4;
                         src_val
@@ -159,16 +109,16 @@ fn main() {
                 };
             }
             0xC2 => {
-                println!("ADD a <- b");
+                // println!("ADD a <- b");
                 r.a = (r.a as u16 + r.b as u16) as u8;
             }
             0xE1 => {
-                println!("APTR imm8");
+                // println!("APTR imm8");
                 r.ptr = r.ptr + (memory[r.pc as usize] as u32);
                 r.pc += 1;
             }
             0xC1 => {
-                println!("CMP");
+                // println!("CMP");
                 if r.a == r.b {
                     r.f = 0;
                 } else {
@@ -176,11 +126,11 @@ fn main() {
                 }
             }
             0x01 => {
-                println!("HALT");
+                // println!("HALT");
                 break;
             }
             0x21 => {
-                println!("JEZ imm32");
+                // println!("JEZ imm32");
                 if r.f == 0 {
                     r.pc = get_imm32(&memory, r.pc);
                 } else {
@@ -188,7 +138,7 @@ fn main() {
                 }
             }
             0x22 => {
-                println!("JNZ imm32");
+                // println!("JNZ imm32");
                 if r.f != 0 {
                     r.pc = get_imm32(&memory, r.pc);
                 } else {
@@ -196,11 +146,11 @@ fn main() {
                 }
             }
             0x02 => {
-                println!("OUT a");
+                // println!("OUT a");
                 output.push(r.a);
             }
             0xC3 => {
-                println!("SUB a <- b");
+                // println!("SUB a <- b");
                 let mut res: i16 = r.a as i16 - r.b as i16;
                 if res < 0 {
                     res += 255;
@@ -208,7 +158,7 @@ fn main() {
                 r.a = res as u8;
             }
             0xC4 => {
-                println!("XOR a <- b");
+                // println!("XOR a <- b");
                 r.a = r.a ^ r.b;
             }
             _ => panic!("Unknown instruction"),
